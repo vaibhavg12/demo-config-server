ed0cb7d92fa34a8a0afdcd3628925d68a41e7072
Vaibhav Gupta <23304655+vaibhavg12@users.noreply.github.com>
2020-05-06 16:00:53 +0800

    Initial commit

---
+++ src/test/java/com/vb/demo/config/server/DemoConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vb.demo.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
